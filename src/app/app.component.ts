4f2ce8ccf950ae273bf779c2a00f1ee8ee58a708
RaulROR <raulror@gmail.com>
2020-02-16 13:11:28 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title   = '029 dirNgMezcla';
+  title   = 'Aprendiendo Directivas';
   mostrar = false;
-  numeros:number [] = [1,2,3,4,5,6];
-  pares:boolean = true; impares:boolean = true;
-  colorPar = 'blue'; colorImpar = 'red';
+  numeros:number [] = [1,2,3,4,5,6,7,8];
+  pares:boolean = true;
+  impares:boolean = true;
+  colorPar = 'blue'; 
+  colorImpar = 'red';
+  
+  
   clActividad(valor){
     let classes = { activo: valor, inactivo: !valor };
     return classes;
